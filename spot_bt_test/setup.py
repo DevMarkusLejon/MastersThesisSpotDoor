ef532bbc44813dc3bed85a8e86f6eb0a8880bb73
Fredrik Sundt <sundtfredrik@gmail.com>
2026-02-09 09:35:54 +0100

    New day, new possibilities

--- spot_bt_test/setup.py
+++ spot_bt_test/setup.py
@@ -1,30 +1,29 @@
+
+from glob import glob
+import os
 from setuptools import find_packages, setup
 
-package_name = 'spot_bt_test'
+PACKAGE_NAME = "spot_bt_test"
 
 setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    name=PACKAGE_NAME,
+    version="0.0.0",
+    packages=[PACKAGE_NAME],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", [f"resource/{PACKAGE_NAME}"]),
+        (f"share/{PACKAGE_NAME}", ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools", "py_trees", "py_trees_os"],
     zip_safe=True,
-    maintainer='sundt',
-    maintainer_email='sundtfredrik@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    maintainer="sundt-lejon",
+    maintainer_email="sundtfredrik@gmail.com",
+    description="Testing the capabilities of spot bt",
+    license="TODO",
+    test_require=["pytest"],
     entry_points={
         'console_scripts': [
 		'stand_walk_sit_bt = spot_bt_test.stand_walk_sit_bt:main',
+		'spot_arm_demo = spot_bt_test.demo_arm:main',
         ],
     },
 )
