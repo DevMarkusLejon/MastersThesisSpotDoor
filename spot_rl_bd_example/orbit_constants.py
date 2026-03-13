c57671de87c8da9ceaa41331929940b65014da7d
themlgbros <markus.lejon99@hotmail.com>
2026-03-13 16:02:19 +0100

    fixed degrees of freedom in their orbit code to include the arm, also added them to the list of constants

---
+++ spot_rl_bd_example/orbit_constants.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2024 Boston Dynamics AI Institute LLC. All rights reserved.
+
+"""spots base joints in order expected/used by orbit libraries"""
+
+ordered_joint_names_orbit = [
+    "fl_hx",
+    "fr_hx",
+    "hl_hx",
+    "hr_hx",
+    "fl_hy",
+    "fr_hy",
+    "hl_hy",
+    "hr_hy",
+    "fl_kn",
+    "fr_kn",
+    "hl_kn",
+    "hr_kn",
+    "a0_sh0",
+    "a0_sh1",
+    "a0_el0",
+    "a0_el1",
+    "a0_wr0",
+    "a0_wr1",
+    "a0_f1x",
+
+]
